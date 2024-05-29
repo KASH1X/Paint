409158efd67947854758e462d6eccb0d726c7e36
Kashix <aleksandraborkowska693@gmail.com>
2024-05-29 21:53:06 +0200

    Dodaj pliki projektów.

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbKolor = new System.Windows.Forms.PictureBox();
+            this.btnOlowek = new System.Windows.Forms.Button();
+            this.btnGumka = new System.Windows.Forms.Button();
+            this.btnElipsa = new System.Windows.Forms.Button();
+            this.btnProstokat = new System.Windows.Forms.Button();
+            this.btnLinia = new System.Windows.Forms.Button();
+            this.btnWyczysc = new System.Windows.Forms.Button();
+            this.pbKarta = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKolor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKarta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbKolor
+            // 
+            this.pbKolor.Location = new System.Drawing.Point(10, 18);
+            this.pbKolor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbKolor.Name = "pbKolor";
+            this.pbKolor.Size = new System.Drawing.Size(51, 47);
+            this.pbKolor.TabIndex = 1;
+            this.pbKolor.TabStop = false;
+            this.pbKolor.Click += new System.EventHandler(this.pbKolor_Click);
+            // 
+            // btnOlowek
+            // 
+            this.btnOlowek.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnOlowek.Location = new System.Drawing.Point(75, 16);
+            this.btnOlowek.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOlowek.Name = "btnOlowek";
+            this.btnOlowek.Size = new System.Drawing.Size(100, 48);
+            this.btnOlowek.TabIndex = 2;
+            this.btnOlowek.Text = "Ołówek";
+            this.btnOlowek.UseVisualStyleBackColor = true;
+            this.btnOlowek.Click += new System.EventHandler(this.btnOlowek_Click);
+            // 
+            // btnGumka
+            // 
+            this.btnGumka.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnGumka.Location = new System.Drawing.Point(200, 15);
+            this.btnGumka.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGumka.Name = "btnGumka";
+            this.btnGumka.Size = new System.Drawing.Size(100, 48);
+            this.btnGumka.TabIndex = 3;
+            this.btnGumka.Text = "Gumka";
+            this.btnGumka.UseVisualStyleBackColor = true;
+            this.btnGumka.Click += new System.EventHandler(this.btnGumka_Click);
+            // 
+            // btnElipsa
+            // 
+            this.btnElipsa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnElipsa.Location = new System.Drawing.Point(327, 15);
+            this.btnElipsa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnElipsa.Name = "btnElipsa";
+            this.btnElipsa.Size = new System.Drawing.Size(100, 48);
+            this.btnElipsa.TabIndex = 4;
+            this.btnElipsa.Text = "Elipsa";
+            this.btnElipsa.UseVisualStyleBackColor = true;
+            this.btnElipsa.Click += new System.EventHandler(this.btnElipsa_Click);
+            // 
+            // btnProstokat
+            // 
+            this.btnProstokat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnProstokat.Location = new System.Drawing.Point(451, 16);
+            this.btnProstokat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnProstokat.Name = "btnProstokat";
+            this.btnProstokat.Size = new System.Drawing.Size(100, 48);
+            this.btnProstokat.TabIndex = 5;
+            this.btnProstokat.Text = "Prostokąt";
+            this.btnProstokat.UseVisualStyleBackColor = true;
+            this.btnProstokat.Click += new System.EventHandler(this.btnProstokat_Click);
+            // 
+            // btnLinia
+            // 
+            this.btnLinia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnLinia.Location = new System.Drawing.Point(576, 16);
+            this.btnLinia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLinia.Name = "btnLinia";
+            this.btnLinia.Size = new System.Drawing.Size(100, 48);
+            this.btnLinia.TabIndex = 6;
+            this.btnLinia.Text = "Linia";
+            this.btnLinia.UseVisualStyleBackColor = true;
+            this.btnLinia.Click += new System.EventHandler(this.btnLinia_Click);
+            // 
+            // btnWyczysc
+            // 
+            this.btnWyczysc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnWyczysc.Location = new System.Drawing.Point(823, 17);
+            this.btnWyczysc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnWyczysc.Name = "btnWyczysc";
+            this.btnWyczysc.Size = new System.Drawing.Size(100, 48);
+            this.btnWyczysc.TabIndex = 8;
+            this.btnWyczysc.Text = "Wyczyść";
+            this.btnWyczysc.UseVisualStyleBackColor = true;
+            this.btnWyczysc.Click += new System.EventHandler(this.btnWyczysc_Click);
+            // 
+            // pbKarta
+            // 
+            this.pbKarta.BackColor = System.Drawing.Color.White;
+            this.pbKarta.Location = new System.Drawing.Point(10, 93);
+            this.pbKarta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbKarta.Name = "pbKarta";
+            this.pbKarta.Size = new System.Drawing.Size(921, 431);
+            this.pbKarta.TabIndex = 11;
+            this.pbKarta.TabStop = false;
+            this.pbKarta.Paint += new System.Windows.Forms.PaintEventHandler(this.pbKarta_Paint);
+            this.pbKarta.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbKarta_MouseDown);
+            this.pbKarta.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbKarta_MouseMove);
+            this.pbKarta.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbKarta_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 550);
+            this.Controls.Add(this.pbKarta);
+            this.Controls.Add(this.btnWyczysc);
+            this.Controls.Add(this.btnLinia);
+            this.Controls.Add(this.btnProstokat);
+            this.Controls.Add(this.btnElipsa);
+            this.Controls.Add(this.btnGumka);
+            this.Controls.Add(this.btnOlowek);
+            this.Controls.Add(this.pbKolor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbKolor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKarta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbKolor;
+        private System.Windows.Forms.Button btnOlowek;
+        private System.Windows.Forms.Button btnGumka;
+        private System.Windows.Forms.Button btnElipsa;
+        private System.Windows.Forms.Button btnProstokat;
+        private System.Windows.Forms.Button btnLinia;
+        private System.Windows.Forms.Button btnWyczysc;
+        private System.Windows.Forms.PictureBox pbKarta;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        Bitmap obraz;
+        Graphics g;
+
+        bool czyMoznaRysowac = false;
+        Point start, koniec;
+        Pen olowek;
+        Pen gumka;
+        ColorDialog oknoKoloru;
+
+        int narzedzie;
+
+        int szerokosc, wysokosc;
+
+        private void btnOlowek_Click(object sender, EventArgs e)
+        {
+            narzedzie = 1;
+        }
+
+        private void btnGumka_Click(object sender, EventArgs e)
+        {
+            narzedzie = 2;
+        }
+
+        private void btnElipsa_Click(object sender, EventArgs e)
+        {
+            narzedzie = 3;
+        }
+
+        private void btnProstokat_Click(object sender, EventArgs e)
+        {
+            narzedzie = 4;
+        }
+
+        private void btnLinia_Click(object sender, EventArgs e)
+        {
+            narzedzie = 5;
+        }
+
+        private void pbKarta_MouseDown(object sender, MouseEventArgs e)
+        {
+            czyMoznaRysowac = true;
+            start = e.Location;
+            startX= e.X;
+            startY= e.Y;
+        }
+
+        private void pbKarta_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(czyMoznaRysowac == true)
+            {
+                if(narzedzie == 1)
+                {
+                    koniec = e.Location;
+                    g.DrawLine(olowek, start, koniec);
+                    start = koniec;
+                    
+                }
+                else if (narzedzie == 2)
+                {
+                    koniec = e.Location;
+                    g.DrawLine(gumka, start, koniec);
+                    start = koniec;
+
+                }
+
+
+            }
+            pbKarta.Refresh();
+            koniecX = e.Location.X;
+            koniecY = e.Location.Y;
+            szerokosc = koniecX - startX;
+            wysokosc = koniecY - startY;
+        }
+
+        private void pbKarta_MouseUp(object sender, MouseEventArgs e)
+        {
+            czyMoznaRysowac = false;
+            if(narzedzie == 3)
+            {
+                g.DrawEllipse(olowek, startX, startY, szerokosc, wysokosc);
+            }else if(narzedzie == 4)
+            {
+                g.DrawRectangle(olowek, startX, startY, szerokosc, wysokosc);
+            }else if (narzedzie == 5)
+            {
+                g.DrawLine(olowek, startX, startY, koniecX,koniecY);
+            }
+        }
+
+        private void pbKolor_Click(object sender, EventArgs e)
+        {
+            oknoKoloru.ShowDialog();
+            olowek.Color = oknoKoloru.Color;
+            pbKolor.BackColor = oknoKoloru.Color;
+        }
+
+        private void btnWyczysc_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pbKarta.Image = obraz;
+        }
+
+        private void pbKarta_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g2 = e.Graphics;
+            if(czyMoznaRysowac == true)
+            {
+                if (narzedzie == 3)
+                {
+                    g2.DrawEllipse(olowek, startX, startY, szerokosc, wysokosc);
+                }
+                else if (narzedzie == 4)
+                {
+                    g2.DrawRectangle(olowek, startX, startY, szerokosc, wysokosc);
+                }
+                else if (narzedzie == 5)
+                {
+                    g2.DrawLine(olowek, startX, startY, koniecX, koniecY);
+                }
+            }
+        }
+
+        int startX, startY, koniecX, koniecY;
+        public Form1()
+        {
+            InitializeComponent();
+
+            obraz = new Bitmap(pbKarta.Width, pbKarta.Height);
+            g = Graphics.FromImage(obraz);
+            pbKarta.Image = obraz;
+            pbKolor.BackColor = Color.Black;
+            olowek = new Pen(Color.Black, 1);
+            gumka = new Pen(Color.White, 10);
+            oknoKoloru = new ColorDialog();
+        }
+
+       
+    }
+}
